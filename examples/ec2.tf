af4f59a2c5c6fd6d228f11f9109c8b634dd40262
sumanth <sumanthpower007@gmail.com>
2023-06-27 23:43:33 +0530

    yaml file

--- examples/ec2.tf
+++ examples/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "app" {
   ami                        = "ami-0c1d144c8fdd8d690"
-  instance_type              = "t3.micro"
+  instance_type              = "t2.micro"
 
   tags = {
-    Name = "MyFirstTerraformInstance"
+    Name = "MyFirstTerraformin"
   }
 }
 
