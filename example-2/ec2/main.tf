79874e4ecb7f93a34d8ff7e31213220f007cf97f
sumanth <sumanthpower007@gmail.com>
2023-06-30 12:17:54 +0530

    yaml file

--- example-2/ec2/main.tf
+++ example-2/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app" {
   ami                        = "ami-0c1d144c8fdd8d690"
   instance_type              = "t2.micro"
-  #  vpc_security_group_ids     = [var.sg]
+   vpc_security_group_ids     = [var.sg]
 
 
   tags = {
--- example-2/main.tf
+++ example-2/main.tf
@@ -1,6 +1,6 @@
 module "ec2" {
     source = "./ec2"
-  
+    SG     = module.sg.sgid
 }
 
 module "sg" {
