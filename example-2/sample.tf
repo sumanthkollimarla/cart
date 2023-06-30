fb27d149f7092b1e8ab8628f3ff038a98265a0b4
sumanth <sumanthpower007@gmail.com>
2023-06-30 11:53:59 +0530

    yaml file

--- example-2/sample.tf
+++ example-2/sample.tf
@@ -1,17 +1,17 @@
-variable "sample" {
-  default  = "Hello World"
-}
-variable "ex-list" {
-  default = [
-    "Cloud",
-    "DevOps",
-    100,
-    false,
-    "Verma",
-    "SRE"
-  ]
-}
+# variable "sample" {
+#   default  = "Hello World"
+# }
+# variable "ex-list" {
+#   default = [
+#     "Cloud",
+#     "DevOps",
+#     100,
+#     false,
+#     "Verma",
+#     "SRE"
+#   ]
+# }
 
-output "ex-list-output" {
-    value = "Welcome to ${var.ex-list[0]} ${var.ex-list[1]} Training and the total course duration is ${var.ex-list[2]} and the trainer name is ${var.ex-list[4]}"
-}
+# output "ex-list-output" {
+#     value = "Welcome to ${var.ex-list[0]} ${var.ex-list[1]} Training and the total course duration is ${var.ex-list[2]} and the trainer name is ${var.ex-list[4]}"
+# }
